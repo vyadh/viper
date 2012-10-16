c2bf41ee708e76aa2029aee274bfa59b0b8edf6d
kieronw <kieron.wilkinson@paretopartners.com>
2012-10-16 11:37:56 +0100

    save frame positions

--- ui/src/viper/ui/UI.scala
+++ ui/src/viper/ui/UI.scala
@@ -2,6 +2,7 @@ package viper.ui
 
 import javax.swing._
 import java.awt.event.{WindowEvent, WindowAdapter}
+import java.util.prefs.Preferences
 
 trait UI extends UIComponents {
 
@@ -20,6 +21,7 @@ trait UI extends UIComponents {
     setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE)
     addWindowListener(new WindowAdapter {
       override def windowClosing(e: WindowEvent) {
+        save()
         close()
       }
     })
@@ -31,13 +33,28 @@ trait UI extends UIComponents {
   }
 
   def initSize() {
-    // todo set from prefs
-    setSize(1000, 600)
+    val width = prefs.getInt(name + ".frame.width", 1000)
+    val height = prefs.getInt(name + ".frame.height", 600)
+    setSize(width, height)
   }
 
   def initPosition() {
-    // todo set from prefs
-    setLocationRelativeTo(null)
+    val x = prefs.getInt(name + ".frame.x", -1)
+    val y = prefs.getInt(name + ".frame.y", -1)
+    if (x == -1 || y == -1) {
+      setLocationRelativeTo(null)
+    } else {
+      setLocation(x, y)
+    }
   }
+
+  def save() {
+    prefs.putInt(name + ".frame.width", getSize.width)
+    prefs.putInt(name + ".frame.height", getSize.height)
+    prefs.putInt(name + ".frame.x", getLocation.x)
+    prefs.putInt(name + ".frame.y", getLocation.y)
+  }
+
+  lazy val prefs = Preferences.userNodeForPackage(getClass)
 
 }
